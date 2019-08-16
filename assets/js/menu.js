726832e8f887144b901337b2f601457c1dcbcc47
Jason Raimondi <jason@raimondi.us>
2019-08-15 21:57:59 -0700

    Allow hide sidebar on large screens.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,22 +1,30 @@
 let isSidebarOpen = false;
 
-// // Toggle button
+const html = document.querySelector('html');
+
+// open sidebar on large screens
+document.addEventListener("DOMContentLoaded", function () {
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (screenWidth > 993) {
+        openSidebar();
+    }
+});
+
+function openSidebar() {
+    html.classList.add('sidebar-is-open');
+    isSidebarOpen = true;
+}
+
+function closeSidebar() {
+    html.classList.remove('sidebar-is-open');
+    isSidebarOpen = false;
+}
+
+// Toggle button
 document.querySelector('#js-toggle-button').addEventListener('click', function () {
     if (isSidebarOpen) {
         closeSidebar();
     } else {
         openSidebar();
     }
-    isSidebarOpen = !isSidebarOpen;
 });
-
-function openSidebar() {
-    const element = document.querySelector('html');
-    element.classList.add('sidebar-is-open');
-}
-
-
-function closeSidebar() {
-    const element = document.querySelector('html');
-    element.classList.remove('sidebar-is-open');
-}
