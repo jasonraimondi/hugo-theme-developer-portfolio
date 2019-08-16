6f5731ae8b268e338d52e6d6361d7ac8aa966f1f
Jason Raimondi <jason@raimondi.us>
2019-08-15 23:00:58 -0700

    Add sidebar toggle bar.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,14 +2,30 @@ let isSidebarOpen = false;
 
 const html = document.querySelector('html');
 
+const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+if (screenWidth > 993) {
+    openSidebar();
+}
+
 // open sidebar on large screens
-document.addEventListener("DOMContentLoaded", function () {
-    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (screenWidth > 993) {
+// document.addEventListener("DOMContentLoaded", function () {
+//
+// });
+
+document.querySelector('#js-toggle-button')
+    .addEventListener('click', toggleSidebar);
+document.querySelector('.menu-toggle')
+    .addEventListener('click', toggleSidebar);
+
+// Toggle button
+function toggleSidebar() {
+    if (isSidebarOpen) {
+        closeSidebar();
+    } else {
         openSidebar();
     }
-});
-
+}
 function openSidebar() {
     html.classList.add('sidebar-is-open');
     isSidebarOpen = true;
@@ -19,12 +35,3 @@ function closeSidebar() {
     html.classList.remove('sidebar-is-open');
     isSidebarOpen = false;
 }
-
-// Toggle button
-document.querySelector('#js-toggle-button').addEventListener('click', function () {
-    if (isSidebarOpen) {
-        closeSidebar();
-    } else {
-        openSidebar();
-    }
-});
